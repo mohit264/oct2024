23963a8c8777d7810d3830ec3a9a630b743d304d
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2024-11-23 19:37:00 +0530

    Added Database access layer and implemented user insert functionality

---
+++ UserReg.py
@@ -0,0 +1,16 @@
+#include <stdio.h> // 'stdio.h' contains declaration of printf()
+// Entry-Point Function => main() => Valid Return Type (int) and 3 Parameters
+//  (int argc, char *argv[], char *envp[])
+int main(int argc, char *argv[], char *envp[])
+// int main(void)
+{ 
+    printf("\n\n");
+
+    return (0);
+}
+
+// Backend Implement Data Access Logic
+void InserUser(User user)
+{    
+    printf("User Inserted in Database\n");
+}
