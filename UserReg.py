17220fb0a432f428351e87a63b6865f49cbb322a
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2024-11-23 19:23:46 +0530

    Written Flask Backend API for user registration

---
+++ UserReg.py
@@ -0,0 +1,19 @@
+#include <stdio.h> // 'stdio.h' contains declaration of printf()
+
+// ls -a
+
+// Entry-Point Function => main() => Valid Return Type (int) and 3 Parameters
+//  (int argc, char *argv[], char *envp[])
+int main(int argc, char *argv[], char *envp[])
+// int main(void)
+{ 
+    printf("\n\n");
+
+    return (0);
+}
+
+// Backend Flask API for user registration
+void UserRegistration(void)
+{    
+    printf("User Successfully Registered\n");
+}
